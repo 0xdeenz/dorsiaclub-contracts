fa30becb2abc49a04a615b2c657e5bc304fc2026
0xdeenz <0xdeenz@protonmail.com>
2023-05-07 22:03:26 +0200

    update config files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,44 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
-    solidity: "0.8.9",
-};
+import "@nomicfoundation/hardhat-chai-matchers"
+import "@nomicfoundation/hardhat-network-helpers"
+import "@nomicfoundation/hardhat-toolbox"
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-etherscan"
+import "@typechain/hardhat"
+import "hardhat-gas-reporter"
+import "hardhat-contract-sizer"
+import "solidity-coverage"
+import { config as dotenvConfig } from "dotenv"
+import { HardhatUserConfig } from "hardhat/config"
+import { resolve } from "path"
+import { config } from "./package.json"
 
-export default config;
+dotenvConfig({ path: resolve(__dirname, "../../.env") })
+
+const hardhatConfig: HardhatUserConfig = {
+    solidity: config.solidity,
+    paths: {
+        sources: config.paths.sources,
+        tests: config.paths.tests,
+        cache: config.paths.cache,
+        artifacts: config.paths.build.contracts
+    },
+    gasReporter: {
+        currency: "USD",
+        enabled: true
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY
+    },
+    mocha: {
+        timeout: 50000
+    },
+    contractSizer: {
+        alphaSort: true,
+        disambiguatePaths: false,
+        runOnCompile: false,
+        strict: true,
+    }
+}
+
+export default hardhatConfig;
