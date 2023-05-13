de731fb8c78a9a698e4ee16ad95c809db0a228c0
0xdeenz <0xdeenz@protonmail.com>
2023-05-13 16:58:22 +0200

    add DCT contract

---
+++ contracts/DorsiaToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IDorsiaClubToken.sol";
+
+contract DorsiaClubToken is IDorsiaClubToken, ERC20Burnable, Ownable {
+    constructor() ERC20("Dorsia Club Token", "DCT") {
+        _mint(msg.sender, DCT_AIRDROP_SUPPLY * 10 ** decimals());
+        _mint(tx.origin, (MAX_DCT_SUPPLY - DCT_AIRDROP_SUPPLY) * 10 ** decimals());
+    }
+}
---
+++ contracts/interfaces/IDorsiaClubToken.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { MAX_DCT_SUPPLY, DCT_AIRDROP_SUPPLY, MAX_SUPPLY } from "../libs/Constants.sol";
+
+interface IDorsiaClubToken is IERC20 {
+
+}
