813e376b78b45567e984d81fbb79ba8fa170e448
0xdeenz <0xdeenz@protonmail.com>
2023-05-13 13:35:26 +0200

    add constants file

---
+++ contracts/libs/Constants.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+// Maximum number of Business Cards there will ever be
+uint256 constant MAX_SUPPLY = 5000;
+// Token mint price
+uint256 constant MINT_PRICE = 0.01 ether;
+// Oracle update transaction gas price
+uint256 constant ORACLE_FEE = 0.0005 ether;
